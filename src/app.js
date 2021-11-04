6c7b9639a1e9ebbc724ee59ecc9e59463e1e4935
Seda Ünal <sedaylmz06@outlook.com>
2021-11-05 01:02:29 +0300

    javascriptexercise

---
+++ src/app.js
@@ -0,0 +1,40 @@
+console.log("Merhaba Kodlama.io")
+
+//type safe değildir!!
+let dolarBugun = 9.30
+
+
+let dolarDun = 9.20
+
+{
+let dolarDun = 9.10
+
+}
+console.log (dolarDun)
+
+const euroDun = 11.2
+//bunu yapamıyoruz hata veriyor
+//euroDun = 11
+
+console.log(euroDun)
+
+
+//camelCasing
+//PascalCasing
+let konutKredileri = ["Konut kredisi", "Emlak konut kredisi","Kamu konut kredisi"]
+console.log("<ul>")
+
+    for (let i = 0; i < 3; i++) {
+            console.log ("<li>" + konutKredileri[i] + "</li>")
+        
+    }
+
+
+("</ul>")
+
+
+
+
+
+
+ 
